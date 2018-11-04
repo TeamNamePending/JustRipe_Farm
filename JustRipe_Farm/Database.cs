509916aa560f0da3fcef406c1e273e98cc5c8740
Ashley <ashleystewart1991@hotmail.co.uk>
2018-11-04 22:55:12 +0000

    Started working on the database class. Creating constructor and methods for closing and opening the database connection

--- JustRipe_Farm/Database.cs
+++ JustRipe_Farm/Database.cs
@@ -16,5 +16,58 @@ namespace JustRipe_Farm
         {
             InitializeComponent();
         }
+
+        private void Database_Load(object sender, EventArgs e)
+        {
+
+
+
+
+
+        }
+
+        private string connectionString;
+
+
+        // acting as the connection to SQL server Database,  the SqlConnection is the constructor, new instance of the class
+        System.Data.SqlClient.SqlConnection connectToDataBase;
+
+
+        // creating an instance of the SqlDataAdapter class- we need  data adapter which enables communcation between dataSet and dataSource
+        System.Data.SqlClient.SqlDataAdapter dataAdapter;
+
+        // set up constructor here
+
+            public void databaseConnection(string connectionString)
+        {
+            this.connectionString = connectionString;
+
+        }
+
+        // Open Database connection
+        public void openConnection()
+        {
+            // creating the Database connection  will be a new instance
+            connectToDataBase = new
+                  System.Data.SqlClient.SqlConnection(connectionString);
+        
+            //  Opening the connection
+            connectToDataBase.Open();
+      
+        }  // end of method for opening the connection
+
+
+        // Close Database connection
+        public void closeConnection()
+        {
+            connectToDataBase.Close();
+
+        }
+
+
+        
+
     }
+    
+
 }
