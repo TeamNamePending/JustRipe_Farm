aab6c988494ac15b88fdb8b28d1e6ad0d34fcd06
Ashley <ashleystewart1991@hotmail.co.uk>
2018-11-06 11:32:50 +0000

    Created the method to obtain the data from SQL statements

--- JustRipe_Farm/Database.cs
+++ JustRipe_Farm/Database.cs
@@ -44,6 +44,8 @@ namespace JustRipe_Farm
 
         }
 
+        // METHODS
+
         // Open Database connection
         public void openConnection()
         {
@@ -64,7 +66,28 @@ namespace JustRipe_Farm
 
         }
 
+        // Method to retrieve the data generated from SQL statements. DataSet class represent in memory collection of data
+        public DataSet getDataSet(string sqlStatement)  // name of method getDataSet
+        {
+           // creating an instance of the DataSet class
+            DataSet dataSet;
 
+            // creating an object to use a table from Database, the DataAdapter will enable communication between datasource and dataset
+            dataAdapter = new System.Data.SqlClient.SqlDataAdapter(sqlStatement, connectToDataBase);
+
+            // creating the Dataset
+            dataSet = new System.Data.DataSet();
+
+            // .Fill method will fill the dataSet with data. It can add or refresh rows, in a specific range in DataSet, to match that in the DataSource
+            //  using DataSet and source table names
+            dataAdapter.Fill(dataSet);
+
+            //  ending the method, return needs to be used otherwise will get an error with the code not returning a value
+            //  as void is not being used,  return needs to be used.
+            return dataSet;
+            
+        // end of method
+        }
         
 
     }
