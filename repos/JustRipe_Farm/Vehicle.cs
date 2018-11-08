83085077afeddce8837ceb32a0c8842cb8ab0074
Aisha Bi <aisha.bi@outlook.com>
2018-11-08 12:08:57 +0000

    login authentication code and changed database from a form to a class

---
+++ repos/JustRipe_Farm/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JustRipe_Farm
+{
+	public partial class Vehicle : Form
+	{
+		public Vehicle()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//this code take the user back to the homepage
+			this.Hide();
+			Homepage page2 = new Homepage();
+			page2.Show();
+		}
+
+	}
+}
