1b7b8f08a4a111ae9dd9b88277df109a0bcb43c2
Ashley <ashleystewart1991@hotmail.co.uk>
2018-11-14 15:16:04 +0000

    added SQL class

--- JustRipe_Farm/SQL.cs
+++ JustRipe_Farm/SQL.cs
@@ -1,20 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace JustRipe_Farm
 {
-    public partial class SQL : Form
+    class SQL
     {
-        public SQL()
-        {
-            InitializeComponent();
-        }
     }
 }
