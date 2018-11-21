8f30e6468dbe01fc28aef0e74162abe6c95d0928
Peter Bond <pb658@student.anglia.ac.uk>
2018-11-21 14:17:04 +0000

    Added user info table for login verification and filled in info on most tables

---
+++ Repos/dbo.Containers.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Containers] (
+    [CotainersId]  INT          NOT NULL,
+    [Type]         VARCHAR (10) NULL,
+    [Size]         VARCHAR (20) NULL,
+    [Availability] CHAR (1)     NULL,
+    [Price]        MONEY        NULL,
+    PRIMARY KEY CLUSTERED ([CotainersId] ASC)
+);
+
