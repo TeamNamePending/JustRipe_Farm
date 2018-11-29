59a5cb81911b04d1abe2214db71790d7a7f7d97f
Aisha Bi <aisha.bi@outlook.com>
2018-11-29 11:56:20 +0000

    made log out and homepage button work for updateVehicleData form

--- Repos/JustRipe_Farm/AddToVehicleData.Designer.cs
+++ Repos/JustRipe_Farm/AddToVehicleData.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.dgvUpdateVehicleData = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvUpdateVehicleData)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,11 +43,33 @@
             this.dgvUpdateVehicleData.TabIndex = 0;
             this.dgvUpdateVehicleData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(577, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Homepage";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(673, 404);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 34);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "Log Out";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // AddToVehicleData
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dgvUpdateVehicleData);
             this.Name = "AddToVehicleData";
             this.Text = "AddToVehicleData";
@@ -57,5 +81,7 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgvUpdateVehicleData;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Repos/JustRipe_Farm/AddToVehicleData.cs
+++ Repos/JustRipe_Farm/AddToVehicleData.cs
@@ -20,14 +20,36 @@ namespace JustRipe_Farm
 
         private void load_dgvUpdateVehicleData()
         {
-            DataSet ds = DatabaseCode._instance().getDataSet("SELECT * FROM Vehicles");
-            dgvUpdateVehicleData.DataSource = ds.Tables[0];
+            //doesnt work
+          //  DataSet ds = DatabaseCode._instance().getDataSet("SELECT * FROM Vehicles");
+           // dgvUpdateVehicleData.DataSource = ds.Tables[0];
         }
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            //doesnt work 
             //DataSet ds = DatabaseCode._instance().getDataSet("SELECT * FROM Vehicles");
             //dgvUpdateVehicleData.DataSource = ds.Tables[0];
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //this code take the user back to the homepage
+            this.Hide();
+            Homepage page2 = new Homepage();
+            page2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            // Confirmation that the user has logged out of the system
+            MessageBox.Show("You have successfully logged out.");
+
+            Login page1 = new Login();
+
+            page1.Show();
+            this.Close();
         }
     }
 }
